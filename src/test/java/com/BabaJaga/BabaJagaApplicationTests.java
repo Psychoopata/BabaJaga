6e834843535a3a59972e6299f8203e9128a5e916
Psychoopata <dtrzpiola28@gmail.com>
2020-06-07 13:37:24 +0200

    Init

---
+++ src/test/java/com/BabaJaga/BabaJagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BabaJaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabaJagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
